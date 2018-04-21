73ea0d4dc62be92d557576568296cdbe6207d7c2
ron <ron@Ronalds-MacBook-Pro.local>
2018-04-21 16:52:54 -0400

    added recipeingredient model and associations

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -2,3 +2,7 @@ class RecipeIngredient < ApplicationRecord
     belongs_to :recipe 
     belongs_to :ingredient
 end
+class RecipeIngredient < ApplicationRecord
+   belongs_to :recipe
+   belongs_to :ingredient
+  end
