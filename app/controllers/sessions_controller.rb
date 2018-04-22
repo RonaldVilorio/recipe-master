881b9ee6df2e89e5311f5b3d12e6cd4a7cd0740a
ron <ron@Ronalds-MacBook-Pro.local>
2018-04-22 18:08:19 -0400

    added has_secure_password on user model/added new,create,destroy actions on session c

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,13 @@
 class SessionsController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+    end
+
+    def destroy
+       session.delete :user_id
+       redirect_to root_url
+    end
 end
