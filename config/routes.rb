16b4d265187cae717252b8a9043103556143a6b0
ron <ron@Ronalds-MacBook-Pro.local>
2018-04-22 15:23:27 -0400

    routes for signup/login/logout/ users and recipes/ root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static#index"
+  resources :recipes
+  resources :users do
+    resources :recipes, only: [:index, :show, :new, :edit, :destroy]
+  end
+  get '/signup', to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
 end
