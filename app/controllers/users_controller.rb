d50e768d793fe85f10d94f407b353314562116b0
ron <ron@Ronalds-MacBook-Pro.local>
2018-04-23 15:28:14 -0400

    got my form to render on new view and partial to work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :edit, :update, :destroy]
     
     def new
+        # binding.pry
         @user = User.new
     end
     def create
