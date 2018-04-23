1f253ca977721268b47928724772aa6ea941b3d1
ron <ron@Ronalds-MacBook-Pro.local>
2018-04-23 11:38:25 -0400

    added actions/views for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    
     def new
+        @user = User.new
     end
     def create
+    end
+    def show
+    end
+    def edit
+    end
+    def update
+    end
+
+    private
+    def set_user
+        @user = User.find(params[:id])
     end
     
 end
