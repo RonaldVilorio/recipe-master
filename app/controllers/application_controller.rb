787ea38796a92e8cb337112207d7794bb67e776b
ron <ron@Ronalds-MacBook-Pro.local>
2018-04-24 18:30:05 -0400

    added before action current_user, require logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    before_action :current_user
+    before_action :require_logged_in, except: [:new, :create, :home]
+    
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
