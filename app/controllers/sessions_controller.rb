01a884b333fadc9de433c564aae406517fb75799
ron <ron@Ronalds-MacBook-Pro.local>
2018-04-24 13:33:37 -0400

    form added, login authentication works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,25 @@
 class SessionsController < ApplicationController
     def new
         @user = User.new
-        # login form
     end
 
     def create
         # login authentication
+        @user = User.find_by(name: session_params[:name])
+        if @user && @user.authenticate(session_params[:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to login_path
+        end
     end
 
     def destroy
        session.delete :user_id
        redirect_to root_url
+    end
+    private
+    def session_params
+        params.require(:user).permit(:name,:password)
     end
 end
