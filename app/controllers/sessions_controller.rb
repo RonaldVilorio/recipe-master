39bbff14555f70e7b80ad670b051396a6a192fad
ron <ronvilorio@gmail.com>
2018-04-28 15:51:29 -0400

    facebook callback being routed to correct action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,12 @@ class SessionsController < ApplicationController
     end
     def omnicreate
         binding.pry
-
+        if auth
+            user = User.find_or_create_by_omniauth(auth_hash)
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        end
+        
     end
 
     def destroy
@@ -23,7 +28,7 @@ class SessionsController < ApplicationController
        redirect_to root_url
     end
     private
-    def auth
+    def auth_hash
         request.env['omniauth.auth']
     end
     def session_params
