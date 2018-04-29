2482fc2b8f760e19a7a6d327df3b8d2a75aaf765
ron <ronvilorio@gmail.com>
2018-04-28 20:50:01 -0400

    logging in through facebook works

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,9 @@ class User < ApplicationRecord
     has_secure_password
     has_many :recipes
     def self.find_or_create_by_omniauth(auth_hash)
-        self.where(uid: auth_hash['uid']).first_or_create do |user| 
+        self.where(email: auth_hash['info']['email']).first_or_create do |user| 
             user.name = auth_hash['info']['name']
-            user.email = auth_hash['info']['email']
             user.password = SecureRandom.hex
         end 
-        binding.pry 
     end
 end
