3fd8f457a7310c13de279a480a38b51063928c89
ron <ronvilorio@gmail.com>
2018-05-06 13:38:11 -0400

    working on creating rating

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,5 +1,7 @@
 class RatingsController < ApplicationController
     def create
+        binding.pry
+        # rating = Rating.new(user_id:current_user.id,recipe_id: params[rating][recipe_id],
         # will redirect to recipe_show_page
     end
 end
