c6133cad95a7fc30531b1b9358617d4fff5d9f60
ron <ronvilorio@gmail.com>
2018-05-06 12:46:37 -0400

    Added select option for rating

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,5 @@
+class RatingsController < ApplicationController
+    def create
+        # will redirect to recipe_show_page
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static#index"
+  resources :ratings, only: [:create]
   resources :recipes, only: [:index,:show,:create]
   resources :ingredients, only: [:index]
   resources :users do
