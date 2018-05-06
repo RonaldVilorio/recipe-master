dcd5d6d58b561e9af6dbef716c76755dcf6649a6
ron <ronvilorio@gmail.com>
2018-05-06 16:24:07 -0400

    working on passing recipe_id,user_id to controller

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,7 +1,12 @@
 class RatingsController < ApplicationController
     def create
         binding.pry
+        # @rating = Rating.new()
         # rating = Rating.new(user_id:current_user.id,recipe_id: params[rating][recipe_id],
+        # redirect_to users_recipe_path(params[user_id],params[recipe_id])
         # will redirect to recipe_show_page
+        # current_user.ratings << rating
+        # recipe.ratings << rating
+
     end
 end
