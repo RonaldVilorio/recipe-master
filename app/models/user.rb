18be2464ee13f50ac68c1e8e846658600fcb1830
ron <ronvilorio@gmail.com>
2018-05-06 09:44:35 -0400

    added recipe_id to ratings and made rating a join table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :recipes
+    has_many :ratings
     def self.find_or_create_by_omniauth(auth_hash)
         self.where(email: auth_hash['info']['email']).first_or_create do |user| 
             user.name = auth_hash['info']['name']
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_06_133643) do
+ActiveRecord::Schema.define(version: 2018_05_06_134249) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_05_06_133643) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "recipe_id"
   end
 
   create_table "recipe_ingredients", force: :cascade do |t|
