6b0d772e39baa61368c99689f32ce78e3dee8226
ron <ronvilorio@gmail.com>
2018-05-07 14:19:52 -0400

    comments are appearing for different user

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,14 +1,15 @@
 class RatingsController < ApplicationController
     def create
-
+        # @rating = current_user.ratings.build(rating_params)
         @rating = Rating.new(rating_params)
+        @rating.save
         current_user.ratings << @rating
-        @recipe = Recipe.find_by(id: params[:rating][:recipe_id])
-        @recipe.ratings << @rating
+        @recipe_show = Recipe.find_by(id: params[:rating][:recipe_id])
+        @recipe_show.ratings << @rating
         @user = User.find_by(id: params[:rating][:user_id])
-        @recipe.save
+        @recipe_show.save
         current_user.save
-        redirect_to user_recipe_path(@user,@recipe)
+        redirect_to user_recipe_path(@user,@recipe_show)
         # make sure this is best way of doing this
 
     end
