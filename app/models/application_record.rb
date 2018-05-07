262fbbe1a8c47683d3fef5b356f3f01e24b6a400
ron <ronvilorio@gmail.com>
2018-05-07 15:49:05 -0400

    average rating added per recipe

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  def average
+    binding.pry
+    number_of_stars = self.stars.count
+    sum_of_all_stars = self.inject(0){|sum,x| sum + x}
+    sum_of_all_stars / number_of_stars
+end
 end
