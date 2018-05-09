29f6d4bbe7042d878130ce4fc6bb94356601d79e
ron <ronvilorio@gmail.com>
2018-05-09 09:36:55 -0400

    added partial to show comments and for rating form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,14 +6,7 @@ module ApplicationHelper
         end
         array.inject(0){|sum,x| sum + x} / array.count
     end
-    def show_comments(recipe)
-        
-        recipe.ratings.map do |rating|
-            user_of_comment = User.find_by(id: rating.user_id)
-            user_of_comment.name 
-            
-        end
-    end
+   
     #  html_safe
     
 end
