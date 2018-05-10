bd3d3665ca49a537fb970611ddf3e73eecfacbc3
ron <ronvilorio@gmail.com>
2018-05-10 17:31:25 -0400

    refactered ratings create a bit

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,20 +1,18 @@
 class RatingsController < ApplicationController
     def create
-        # @rating = current_user.ratings.build(rating_params)
-        @rating = Rating.create(rating_params)
+        @rating = current_user.ratings.create(rating_params)
+        # @rating = Rating.create(rating_params)
         # 
         # @rating = Rating.new(rating_params)
         # @rating.save
         # 
-        current_user.ratings << @rating
+        # current_user.ratings << @rating
         @recipe_show = Recipe.find_by(id: params[:rating][:recipe_id])
         @recipe_show.ratings << @rating
         @user = User.find_by(id: params[:rating][:user_id])
         @recipe_show.save
-        current_user.save
         redirect_to user_recipe_path(@user,@recipe_show)
         # refactor
-        Recipe.joins(:ratings).order("stars").distinct
     end
     def rating_params
         params.require(:rating).permit(:comment,:stars,:recipe_id,user_id: current_user.id)
